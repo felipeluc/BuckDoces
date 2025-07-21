1d3babe781dfd59b21e0fd74f69e9f90ac6f5db7
felipeluc <felipelucaspereira246@gmail.com>
2025-07-20 23:33:08 -0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brown: {
+          800: "#4B2E2E",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
